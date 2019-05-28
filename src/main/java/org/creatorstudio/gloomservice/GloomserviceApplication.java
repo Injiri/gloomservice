1f83c1d4acdba9af0b748cf42da6455ba67366cd
Injiri <simoninjiri@gmail.com>
2019-05-28 19:15:15 +0300

    initial commit , configuration

---
+++ src/main/java/org/creatorstudio/gloomservice/GloomserviceApplication.java
@@ -0,0 +1,13 @@
+package org.creatorstudio.gloomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloomserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloomserviceApplication.class, args);
+	}
+
+}
